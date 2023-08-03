9ee6f4d75d821b4a6cffb04a688f63dcc14394bb
cinnData <canela.miguel@icloud.com>
2023-08-03 12:24:06 +0200

    Update web-06p3.py

--- Code/web-06p3.py
+++ Code/web-06p3.py
@@ -110,6 +110,6 @@ for i in range(1, 451):
 data = [data1[i] + data2[i] for i in range(451)]
 
 # Writing to CSV file (edit path) #
-with open('trip.csv', 'w', newline='') as conn:
+with open('trip.csv', 'w', newline='', encoding='utf-8') as conn:
 	writer = csv.writer(conn, delimiter=',')
 	writer.writerows(data)
